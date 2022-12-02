c27b55c5ce8f75807fb80de04a703e6668116cb8
Cheese Connoisseur <tom.sabadin2006@gmail.com>
2022-12-02 22:35:04 +0100

    advent of code 2022

---
+++ day1/CalorieCounting.java
@@ -0,0 +1,41 @@
+package tom.adventOfCode.day1;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CalorieCounting
+{
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\tomsa\\git\\tom\\chess\\src\\main\\java\\tom\\adventOfCode\\day1\\calorie_counting_input.txt"));
+
+        List<Integer> topCalories = new ArrayList<>(3);
+        for (int i = 0; i < 3; i++) topCalories.add(0);
+
+        AtomicInteger topCaloriesTotal = new AtomicInteger();
+        int calories = 0;
+
+        for (String line : lines) {
+            if (!line.isEmpty()) calories += Integer.parseInt(line);
+            else {
+                for (int topCalorie : topCalories) if (calories > topCalorie) {
+                    replaceLowest(topCalories, calories);
+                    break;
+                }
+                calories = 0;
+            }
+        }
+
+        topCalories.forEach(topCaloriesTotal::addAndGet);
+        System.out.println(topCaloriesTotal);
+    }
+
+    public static void replaceLowest(List<Integer> topCalories, int replaceWith) {
+        final Integer[] lowest = {topCalories.get(0)};
+        topCalories.forEach(calorie -> lowest[0] = Math.min(lowest[0], calorie));
+        topCalories.remove(lowest[0]);
+        topCalories.add(replaceWith);
+    }
+}
