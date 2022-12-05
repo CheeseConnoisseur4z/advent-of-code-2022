f5486fa2e6ce5d20bb4e3a66bcc7330ff8ae89c3
Cheese Connoisseur <tom.sabadin2006@gmail.com>
2022-12-05 15:20:53 +0100

    day 3, 4

---
+++ day3/RucksackReorganization.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RucksackReorganization
+{
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\tomsa\\git\\tom\\chess\\src\\main\\java\\tom\\adventOfCode\\day3\\rucksack_reorganization_input.txt"));
+        List<Character> charactersOfLine = new ArrayList<>();
+        List<Character> groupType= new ArrayList<>();
+        int total = 0;
+        int count = 2;
+        char group;
+
+        for (String line : lines) {
+            count++;
+            
+            if (count % 3 == 0) {
+                charactersOfLine.clear();
+                for (int i = 0; i < line.length(); i++) {
+                    if (!charactersOfLine.contains(line.charAt(i))) charactersOfLine.add(line.charAt(i));
+                }
+                groupType = new ArrayList<>(charactersOfLine);
+            } 
+            else 
+            {
+                for (Character character : charactersOfLine) {
+                    if (!line.contains(character + "")) groupType.remove(character);
+                }
+                if (groupType.size() == 1) {
+                    group = groupType.get(0);
+                    if (!Character.isUpperCase(group)) total += group - 96;
+                    else {
+                        total += group - 38;
+                    }
+                }
+            }
+        }
+
+        System.out.println(total);
+    }
+}
---
+++ day4/CampCleanup.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CampCleanup
+{
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\tomsa\\git\\tom\\chess\\src\\main\\java\\tom\\adventOfCode\\day4\\camp_cleanup_input.txt"));
+
+        List<Integer> first = new ArrayList<>();
+        List<Integer> second = new ArrayList<>();
+
+        String[] split;
+        int result = 0;
+        for (String line : lines) {
+            split = line.split(",");
+            addNumbers(split, 0, first);
+            addNumbers(split, 1, second);
+            for (Integer n : first) if (second.contains(n)) {
+                result++;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    static void addNumbers(String[] split, int i, List<Integer> arrayList) {
+        arrayList.clear();
+        for (int n = Integer.parseInt(split[i].split("-")[0]); n <= Integer.parseInt(split[i].split("-")[1]); n++) {
+            arrayList.add(n);
+        }
+    }
+}
